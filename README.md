a51704387795194c50e7aa9697b43e584e20f9dc
Clément Pavué <clement.pavue@gmail.com>
2025-01-08 14:58:48 +0100

    Setup instruction

---
+++ README.md
@@ -0,0 +1,12 @@
+# Setup instrction
+
+```sh
+cp conf.example.json conf.json
+npm i
+```
+
+# Run time instruction
+
+```sh
+npm run release
+```
--- src/modules/events/domain/mappers.ts
+++ src/modules/events/domain/mappers.ts
@@ -1,10 +1,8 @@
 import { Event } from "modules/events/domain/models";
 import moment from "moment";
-import { log } from "tools/utils/log.utils";
 
 
 export function mapEvent(event: Event) {
-    log(`Mapping event ${JSON.stringify(event)}`);
     return {
         name: event.event_title,
         country: event.event_country,
